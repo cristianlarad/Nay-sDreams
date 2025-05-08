12b8f7cf454a57a0627382ef2414998560dfee89
cristianlarad <die.hts1@gmail.com>
2025-05-08 00:05:48 -0400

    fix editbutton

--- src/components/products/EditButton.tsx
+++ src/components/products/EditButton.tsx
@@ -1,5 +1,5 @@
 import pb from "@/utils/instancePocketbase";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
